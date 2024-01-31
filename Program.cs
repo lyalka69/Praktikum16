2c7e8b099ba325b67af5534b321ab8ab22855de7
lyalka69 <l.y.a.l.k.a7.12.7@gmail.com>
2024-01-31 22:05:11 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktikum16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Вывести в строку десять заглавных букв "а" через один пробел:а а а а а а а а а а
+
+            //int a = 1;
+            //while (a <= 10) 
+            //{
+            //    Console.Write($"\t А");
+            //    a++;
+            //}
+            //Console.ReadKey();
+
+            //2. Вывести в обратном порядке числа от 50 до 40.
+
+            //int num2 = 50;
+            //while (num2 >= 40)
+            //{
+            //    Console.WriteLine($"{num2}");
+            //    num2--;
+            //}
+            //Console.ReadKey();
+
+            //3. Вывести в строку через пробел следующие натуральные числа:6    12     24      30      36
+
+            //for (int num1 = 6; num1 <= 36; num1 += 6) 
+            //{
+            //    if (num1 != 18)
+            //    {
+            //        Console.Write($"\t{num1}");
+            //    }
+            //}
+            //Console.ReadKey();
+
+            //4. Вывести двузначные числа, кратные 11, подсчитать их количество и сумму.
+
+            //int c = 0, summa = 0;
+            //for (int num2 = 10;num2 <= 99;num2++)
+            //    if (num2 % 11 == 0)
+            //    {
+            //    Console.WriteLine(num2);    
+            //    c++;
+            //    summa = summa + num2;
+            //    } 
+            //Console.WriteLine($"c = {c} \tsumma = {summa}");
+            //Console.ReadKey();
+
+
+
+        }
+    }
+}
